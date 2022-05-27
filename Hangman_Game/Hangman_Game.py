6087fc8ea7f55be051eecaa6205ec0aacfdb06d7
Abhijit Venkatesh <95538488+Abjt03@users.noreply.github.com>
2022-05-28 01:02:30 +0530

    Add files via upload

--- Hangman_Game/Hangman_Game.py
+++ Hangman_Game/Hangman_Game.py
@@ -6,14 +6,17 @@ import random as rdm
 
 root = Tk()
 root.title('Hangman Game')
-root.geometry('800x500+400+200')
+root.geometry('900x500+300+100')
 root.config(bg='lightgreen')
 root.iconbitmap('Hangman_Icon.ico')
 
 # Functions - Hangman Game
 
 word_list = ['vouch', 'glass', 'scrap', 'gamer', 'olive', 'royal', 'natal', 'epoxy', 'india', 'tipsy', 'dodge', 'cynic', 'ultra']
-photos = [PhotoImage(file="images/9th.png"), PhotoImage(file="images/8th.png"), PhotoImage(file="images/7th.png"), PhotoImage(file="images/6th.png"), PhotoImage(file="images/5th.png"), PhotoImage(file="images/4th.png"), PhotoImage(file="images/3rd.png"), PhotoImage(file="images/2nd.png"), PhotoImage(file="images/1st.png"), PhotoImage(file="images/0th.png")]
+word_list = [x.upper() for x in word_list]
+print(word_list)
+photos = [PhotoImage(file="images/0th.png"), PhotoImage(file="images/1st.png"), PhotoImage(file="images/2nd.png"), PhotoImage(file="images/3rd.png"), PhotoImage(file="images/4th.png"), PhotoImage(file="images/5th.png"), PhotoImage(file="images/6th.png"), PhotoImage(file="images/7th.png"), PhotoImage(file="images/8th.png"), PhotoImage(file="images/9th.png")]
+k = 0
 
 def chooseword():
     global word_list
@@ -22,44 +25,87 @@ def chooseword():
     global word_chosen
     word_chosen = rdm.choice(word_list)
     word_found = ['*' for i in word_chosen]
-    rem_guesses = 2 * len(word_chosen) + 2
-    # Incomplete (maybe)
+    rem_guesses = 2 * len(word_chosen)
     
 def getguess():
     global rem_guesses
     global word_found
     global word_chosen
+    global k
     
     word_str = ''.join(word_found)
     wordLabel.config(text = word_str)
     
     if rem_guesses > 0:
-        guess_entered = list(guess.get())
-        if len(guess_entered) == 1:
-            pass
-        elif len(guess_entered) == len(word_chosen):
-            pass
+        guess_entered = guess.get()
+        
+        if len(guess_entered) == len(word_chosen):
+            if guess_entered.upper() == word_chosen:
+                word_found = word_chosen
+                wordLabel.config(text = word_found)
+                msgLabel.config(text = 'You found it !!!')
+                showinfo('Hangman Game Over', 'Congratulations ... You have won !!!')
+                rem_guesses = 0
+                k = 1
+            else:
+                msgLabel.config(text = 'Nope, try a different word ...')
+                rem_guesses -= 1
+        
+        elif len(guess_entered) == 1:
+            guess_entered = guess_entered.upper()
+            word_chosen_list = list(word_chosen)
+            if guess_entered in word_chosen_list:
+                word_found = [i if guess_entered == i else '*' for i in word_chosen_list]
+                if word_found == word_chosen:
+                    showinfo('Hangman Game Over', 'Congratulations ... You have won !!!')
+                    rem_guesses = 0
+                    k = 1
+                else:
+                    msgLabel.config(text = 'Nice one ...')
+            else:
+                msgLabel.config(text = f'Sorry "{guess_entered}" is not in the word ...')
+                rem_guesses -= 1
+        
+        elif len(guess_entered) == 0:
+            msgLabel.config(text = 'Please enter valid input ...')
+        
         else:
             msgLabel.config(text = 'Wrong Number of Letters ...')
             rem_guesses -= 1
+            
+        if rem_guesses <= 9 and not k:
+            imgLabel.config(image = photos[rem_guesses])
+        
+        if rem_guesses == 0 and not k:
+            guessesLeft.config(text = f'Guesses Left : {rem_guesses}')
+            showinfo('Hangman Game Over', 'Sorry, You ran out of guesses ... Please Restart the game')
+        
     else:
-        showinfo('Hangman Game Over', 'Sorry, You ran out of guesses ... Please try Again')
+        word_found = word_chosen
+        if not k:
+            showinfo('Hangman Game Over', 'Sorry, You ran out of guesses ... Please Restart the game')
+        else:
+            showinfo('Hangman Game Over', 'You already won ... Restart the game')
+        
     
     guessesLeft.config(text = f'Guesses Left : {rem_guesses}')
+    word_str = ''.join(word_found)
+    wordLabel.config(text = word_str)
     # Incomplete
     # Button Command
 
 # Function Call
 
 chooseword()
+word_str = ''.join(word_found)
 
 # Labels
 
 introLabel = Label(root, text = 'Welcome to Hangman Game', font = ('arial', 35, 'bold'), bg = 'lightgreen', fg = 'red')
 introLabel.pack()                 
 
-wordLabel = Label(root, font = ('arial', 55, 'bold'), bg = 'lightgreen', fg = 'red')
-wordLabel.place(relx = 0.55, rely = 0.3, anchor = 'ne')
+wordLabel = Label(root, text = word_str, font = ('arial', 55, 'bold'), bg = 'lightgreen', fg = 'red')
+wordLabel.place(relx = 0.58, rely = 0.3, anchor = 'ne')
 
 infoLabel = Label(root, text = 'Enter a letter or the guessed word :', font = ('arial', 20), bg = 'lightgreen', fg = 'red')
 infoLabel.place(relx = 0.75, rely = 0.5, anchor = 'ne')
